0d10ac15d163458efc748db1fbd6892a5480880b
Luis Moreno <chlodovech@192.168.1.67>
2024-11-24 11:56:49 -0600

    Add Intro component

---
+++ digital-card/src/components/Intro/Intro.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Intro() {
+    return (
+        <div className="intro-section">
+            <h1 className="user-name">Luis Moreno</h1>
+            <h3 className="user-role">Frontend Developer</h3>
+            <a className="portfolio-link" href="https://github.com/mandelbrojt" target="_blank">
+                <p className="short-link">My Portfolio</p>
+            </a>
+        </div>
+    )
+}
