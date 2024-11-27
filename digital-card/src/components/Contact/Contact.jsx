3216c51e63d1224e9c6b93b92b4742e75c5ee915
Luis Moreno <chlodovech@Luiss-MacBook-Air.local>
2024-11-27 14:11:46 -0600

    Add utility class to contact buttons

--- digital-card/src/components/Contact/Contact.jsx
+++ digital-card/src/components/Contact/Contact.jsx
@@ -5,11 +5,11 @@ import LinkedinIcon from "../../assets/linkedin-icon.png"
 export default function Contact() {
     return (
         <div className="contact-section">
-            <button className="email-button">
-                <img src={MailIcon} alt="Email icon" />
+            <button className="email-button contact-button">
+                <img className="email-icon" src={MailIcon} alt="Email icon" />
                 <p className="mail-text">Email</p>
             </button>
-            <button className="linkedin-button">
+            <button className="linkedin-button contact-button">
                 <img src={LinkedinIcon} alt="LinkedIn icon" />
                 <p className="linkedin-text">LinkedIn</p>
             </button>
