c13d1730c56b22ba661f668da189b0dd0aea7bf8
Luis Moreno <chlodovech@Luiss-MacBook-Air.local>
2024-11-27 14:12:48 -0600

    Update user name

--- digital-card/src/components/Intro/Intro.jsx
+++ digital-card/src/components/Intro/Intro.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Intro() {
     return (
         <div className="intro-section">
-            <h1 className="user-name">Luis Moreno</h1>
+            <h1 className="user-name">Luigi</h1>
             <h3 className="user-role">Frontend Developer</h3>
             <a className="portfolio-link" href="https://github.com/mandelbrojt" target="_blank">
                 <p className="short-link">My Portfolio</p>
