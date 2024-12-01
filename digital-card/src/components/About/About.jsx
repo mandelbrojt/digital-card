c75083242bc38b9dbcc36bff9da52df479f9a8b5
Luis Moreno <luis.moreno@exatec.tec.mx>
2024-11-30 20:24:18 -0600

    Add About component

---
+++ digital-card/src/components/About/About.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+function AboutInfo() {
+    return (
+        <div className="about-info">
+            <h3></h3>
+        </div>
+    )
+}
+
+function InterestsInfo() {
+    return (
+        <div className="interests-info"></div>
+    )
+}
+
+export default function About() {
+    return <></>
+}
